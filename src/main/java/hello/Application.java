ed120a83dbce7a203937f8bf8d6f28aa6c22a32b
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-07-30 11:45:36 -0700

    accessing data with MongoDB

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +13,41 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguratio
 @EnableMongoRepositories
 @Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private CustomerRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        repository.save(new Customer("Alice", "Smith"));
+        repository.save(new Customer("Bob", "Smith"));
+
+        System.out.println("Customers found with findAll:");
+        System.out.println("--------------------------------");
+
+        for(Customer customer: repository.findAll()) {
+            System.out.println(customer);
+        }
+
+        System.out.println();
+
+        System.out.println("Customer found with findByFirstNAme('Alice')");
+        System.out.println("-----------------------------------------------");
+        System.out.println(repository.findByFirstName("Alice"));
+
+        System.out.println("Customer found with findByLastName('Smith')");
+        System.out.println("--------------------------------------------");
+
+        for (Customer customer : repository.findByLastName("Smith")){
+            System.out.println(customer);
+        }
     }
 }
